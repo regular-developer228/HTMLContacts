3c33d18fc7eb37bc20e54bc362433ddf8acfb4e0
regular-developer228 <golovkokir22@gmail.com>
2026-01-10 08:15:59 -0800

    created models and form

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=50),
+    phone = models.CharField(max_length=15),
+    mail = models.EmailField(max_length=50),
+    photo = models.ImageField(
+        upload_to = "contact_photo/",
+        default = "default.png",
+        blank = True,
+        null = True
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.phone} {self.mail}"
